d0f9272f0848aa98cb3b110795826618c0f4ba75
ildemar218 <ildemar321@gmail.com>
2026-07-30 00:53:22 -0500

    feat: configurar router y crear vistas iniciales

---
+++ frontend/src/views/auth/LoginView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container mt-5">
+      <h1>Iniciar Sesión</h1>
+      <p>Bienvenido al sistema de gestión de la Cacharrería Mijaid.</p>
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+  </style>
